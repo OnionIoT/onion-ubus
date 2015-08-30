f4a21a7193ff1c42c9567c7eb929efb92b265e21
greenbreakfast <lazardemin@gmail.com>
2015-08-31 04:29:24 +0800

    Changed wifiscan: output results array now has objects with ssid and encryption type for each network

--- onion-ubus.sh
+++ onion-ubus.sh
@@ -6,19 +6,38 @@
 # function to scan for wifi networks
 #	argument 1: device for iwinfo
 WifiScan () {
-	# scan for networks and do some formatting to isolate the ssid's                                     
-	networks=$(iwinfo $1 scan | grep ESSID | sed -e 's/[[:space:]]//g' -e 's/ESSID\://g' -e 's/\"//g')
-	
+	# scan for networks and do some formatting to isolate the ssid and encryption type
+	#	networks looks like ssid1:encr1;ssid2:encr2;ssid3:encr3
+	networks=$(iwinfo $1 scan | grep 'ESSID\|Encryption' | awk '{printf "%s", $0; if (getline) print " " $0; else printf "\n"}' | sed -e 's/[[:space:]]*E/E/g' -e 's/\"//g' -e 's/ESSID\: //g' -e 's/Encryption\: /:/g' -e 's/$/;/' |  sed -e ':a;N;$!ba;s/\n//g')
+
 	# json setup      
 	json_init
 	
+	# create the results array
 	json_add_array results
-	for id in $networks
+
+	# split the list of networks
+	rest=$networks
+	while [ "$rest" != "" ]
 	do
-		json_add_string "" "$id"
+		val=${rest%%;*}
+		rest=${rest#*;}
+
+		#val now holds ssid:encr
+		ssid=${val%%:*}
+		auth=${val#*:}
+
+		# create and populate object for this network
+		json_add_object
+		json_add_string "ssid" "$ssid"
+		json_add_string "encryption" "$auth"
+		json_close_object
 	done
+
+	# finish the array
 	json_close_array
-	
+
+	# print the json
 	json_dump
 }
 
